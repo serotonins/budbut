97988f19b168f747e48439fe75a4d285f3a2a650
serot <serotonins023@gmail.com>
2025-04-17 14:39:02 +0900

    initial commit

---
+++ src/main/java/com/serot/budbut/BudbutApplication.java
@@ -0,0 +1,13 @@
+package com.serot.budbut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudbutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudbutApplication.class, args);
+	}
+
+}
